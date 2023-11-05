a36a208c5dd40abcf6a8d2faa29bbd6e7c91310e
Keisuke Ota <k.ota109@outlook.com>
2023-11-06 00:47:29 +0900

    Create Project

---
+++ src/test/java/com/ot/api/data/station/StationDataApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ot.api.data.station;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StationDataApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
